693e648579cc58eee093da275ae68f2f19c2040b
Mikhail Aleksandrov <al-mn@yandex.ru>
2020-06-25 17:22:35 +0300

    add play sound function

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
 import os
+import vlc
 import logging
 
 logging.basicConfig(level=logging.DEBUG,
     format='%(asctime)s - %(levelname)s - %(message)s')
-#logging.disable(logging.CRITICAL)
+logging.disable(logging.CRITICAL)
 logging.debug('Start of program')
 
-file_path = os.path.dirname(__file__) + '/bird-data.txt'
+my_dir = os.path.dirname(__file__)
 
-logging.debug('file_path: ' + file_path)
+BIRD_DATA = my_dir + '/bird-data.txt'
+logging.debug('BIRD_DATA: ' + BIRD_DATA)
+
+def play_sound(num, audio_dir = my_dir + '/media/audio/'):
+    """Play mp3 file from audio directory"""
+    try:
+        fnames = os.listdir(audio_dir)
+        file = [x for x in fnames if num.zfill(3) in x][0]
+        logging.debug('Playing mp3: ' + audio_dir + file)
+        print(f'Воспроизводится файл {file}')
+        p = vlc.MediaPlayer(audio_dir + file)
+        p.audio_set_volume(50)
+        p.play()
+    except IndexError as error:
+        logging.debug('ERROR:', error)
+        print("Файл не найден. Ошибка:", error)
 
 
 def get_content(file_path):
@@ -31,17 +47,15 @@ while True:
     if to_find == 'end':
         logging.debug('end of script')
         break
-    for i in find_items(to_find, get_content(file_path)):
+    for i in find_items(to_find, get_content(BIRD_DATA)):
         print(i)
     logging.debug('search ended')
     print('Поиск закончен.')
     user_input = input('Введите номер, чтобы воспроизвести аудио: ')
     if user_input == '':
         continue
-    try: 
-        audio_num = int(user_input)
-        print(f'Воспроизводится mp3 номер {audio_num}')
-    except: continue
+
+    play_sound(user_input)    
 
 print('Спасибо за использование Поиска Птиц! До свидания!')
 logging.debug('End of program')
