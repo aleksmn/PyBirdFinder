0a8a55ae76e10a724c8c58206a219d4b109bfe68
Mikhail Aleksandrov <al-mn@yandex.ru>
2020-07-06 15:15:50 +0300

    add image finder

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import vlc
 import logging
+import webbrowser
 
 logging.basicConfig(level=logging.DEBUG,
     format='%(asctime)s - %(levelname)s - %(message)s')
@@ -14,18 +15,19 @@ logging.debug('BIRD_DATA: ' + BIRD_DATA)
 
 def play_sound(num, audio_dir = my_dir + '/media/audio/'):
     """Play mp3 file from audio directory"""
-    try:
-        fnames = os.listdir(audio_dir)
-        file = [x for x in fnames if num.zfill(3) in x][0]
-        logging.debug('Playing mp3: ' + audio_dir + file)
-        print(f'Воспроизводится файл {file}')
-        p = vlc.MediaPlayer(audio_dir + file)
-        p.audio_set_volume(50)
-        p.play()
-    except IndexError as error:
-        logging.debug('ERROR:', error)
-        print("Файл не найден. Ошибка:", error)
 
+    fnames = os.listdir(audio_dir)
+    file = [x for x in fnames if num.zfill(3) in x][0]
+    logging.debug('Playing mp3: ' + audio_dir + file)
+    print(f'Воспроизводится файл {file}')
+    p = vlc.MediaPlayer(audio_dir + file)
+    p.audio_set_volume(50)
+    p.play()
+
+
+def open_images(string_to_search):
+    s = 'https://duckduckgo.com/?t=lm&q=' + string_to_search + '&ia=images&atb=v182-1&iax=images'
+    webbrowser.open(s)
 
 def get_content(file_path):
     """Get list of lines from the file"""
@@ -44,7 +46,7 @@ print('Здравствуйте, это Поиск Птиц!')
 while True:
     to_find = input('Найти: ')
     logging.debug('input: ' + to_find)
-    if to_find == 'end':
+    if to_find == 'q':
         logging.debug('end of script')
         break
     for i in find_items(to_find, get_content(BIRD_DATA)):
@@ -54,8 +56,21 @@ while True:
     user_input = input('Введите номер, чтобы воспроизвести аудио: ')
     if user_input == '':
         continue
+    else:
+        try:
+            item = find_items(user_input, get_content(BIRD_DATA))[0]
+            print('Выбрана запись', item)
+            play_sound(user_input)
+        except IndexError as error:
+            print("Файл не найден. Ошибка:", error)
 
-    play_sound(user_input)    
+        yn = input('Искать фото в интернете? (д/н) ')
+        if yn.upper() == 'Д':
+            string_to_search = item.split(' - ')[1]
+            open_images(string_to_search)
+        else:
+            continue 
+    
 
 print('Спасибо за использование Поиска Птиц! До свидания!')
 logging.debug('End of program')
